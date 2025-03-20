527f8b1b3bb099a2e5e8798e43d57cf9c85b09e7
Akkipr <akki46884688@gmail.com>
2025-03-20 01:55:51 -0400

    Initial Commit

---
+++ ToDoRCat/UserInfo.swift
@@ -0,0 +1,18 @@
+//
+//  UserInfo.swift
+//  ToDoRCat
+//
+//  Created by Akshayan on 2025-03-21.
+//
+
+import SwiftUI
+
+struct UserInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserInfo()
+}
